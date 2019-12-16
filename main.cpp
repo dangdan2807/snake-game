5e4fc738f7fe8a1226508c733449c9d4f5c3ebfd
Phạm Đăng Đan <dangdan2807@gmail.com>
2019-12-16 08:42:54 +0000

    feat: Add function to the wall (them ham ve tuong)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
-#include "console.h"
+//#include "console.h"
 using namespace std;
+//Khai báo các hàm cần dùng
+void ve_tuong_tren();
+void ve_tuong_duoi();
+void ve_tuong_trai();
+void ve_tuong_phai();
+void ve_tuong();
 
 int main() {
-  cout << "Hello World!\n";
+  ve_tuong();
+  _getch();
+}
+
+//Định nghĩa các hàm đã khao báo ở trên
+
+void ve_tuong_tren()
+{
+  int x = 10, y = 1;
+  gotoXY(x, y);
+  while(x<=100){
+    cout <<"+";
+    x++;
+  }
+}
+
+void ve_tuong_duoi()
+{
+  int x = 10, y = 26;
+  gotoXY(x, y);
+  while(x<=100){
+    cout <<"+";
+    x++;
+  }
+}
+
+void ve_tuong_trai()
+{
+  int x = 10, y=1;
+  gotoXY(x, y);
+  while(y<=26){
+    cout <<"+";
+    y++;
+  }
+}
+
+void ve_tuong_phai()
+{
+  int x = 100, y=1;
+  gotoXY(x, y);
+  while(y<=26){
+    cout <<"+";
+    y++;
+  }
+}
+
+void ve_tuong()
+{
+  ve_tuong_tren();
+  ve_tuong_duoi();
+  ve_tuong_trai();
+  ve_tuong_phai();
 }
