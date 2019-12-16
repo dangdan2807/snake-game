cf6cc4028da4a6a824b128fbd2f3a52086e9c25f
dangdan2807 <dangdan2807@gmail.com>
2019-12-17 01:18:52 +0700

    feat: add snake handling function (them ham xu li ran)
    Lam ran di chuyen va chinh sua ran cho hoan thien.
    Edit for the snake to be more complete and move it 1 way.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 #define MAX 100;
+int sl = 4;
 //Khai báo các hàm cần dùng
 void ve_tuong_tren();
 void ve_tuong_duoi();
@@ -11,17 +12,76 @@ void ve_tuong_phai();
 void ve_tuong();
 void ve_ran();
 void khoi_tao_ran();
+void xoa_du_lieu_cu(int toadox[], int toadoy[]);
+void xu_li_ran(int toadox, int toadoy, int x, int y);
+void them(int toado, int vt);
+void xoa(int toado, int vt);
 
 int main() {
   int toadox[MAX], toadoy[MAX];
   ve_tuong();
   khoi_tao_ran(toadox, toadoy);
   ve_ran(toadox, toadoy);
+  int x= 50, y = 13;
+  while(true)
+  {
+  	//xóa vị trí cũ (backspace)
+  	xoa_du_lieu_cu(toadox, toadoy);
+  	int check = 2;
+  	//0: di chuyển xuống
+  	//1: di chuyển lên
+  	//2: di chuyển sang phải
+  	//3: di chuyển sang trái
+  	if(check == 0)
+  	{
+  		
+  	}
+  	else if(check == 1)
+  	{
+  	
+  	}
+  	else if(check == 2)
+  	{
+  	
+  	}
+  	else if(check == 3)
+  	{
+  	
+  	}
+  	xu_li_ran(toadox, toadoy, x, y);
+  	sleep(150);
+  }
   _getch();
 }
 
 
 //Định nghĩa các hàm đã khao báo ở trên
+void xu_li_ran(int toadox, int toadoy, int x, int y)
+{
+	//b1: thêm tọa độ mới vào đầu mảng
+	them(toadox, x);
+	them(toadoy, y);
+	//b2: xóa tọa độ cuối mảng
+	xoa(toadox, sl - 1);
+	xoa(toadoy, sl - 1);
+	//b3: vẽ lại rắn mới
+	ve_ran(toadox, toadoy);
+}
+void them(int toado, int vt)
+{
+	for(int i = sl; i > 0; i--){
+		a[i] = a[i - 1];
+	}
+	a[0] = vt;
+	sl++;
+}
+void xoa(int toado, int vt)
+{
+	for(int i = vt; i < sl; i++){
+		a[i] = a[i + 1];
+	}
+	sl--;
+}
 void khoi_tao_ran(int toadox[], int toadoy[])
 {
   int x=50, y=13;
@@ -30,7 +90,6 @@ void khoi_tao_ran(int toadox[], int toadoy[])
     toadiy[i] = y;
   }
 }
-
 void ve_ran(int toadox[], toadoy[])
 {
   for(int i= 0; i< sl; i++){
@@ -43,6 +102,14 @@ void ve_ran(int toadox[], toadoy[])
     }
   }
 }
+void xoa_du_lieu_cu(int toadox[], int toadoy[])
+{
+	for(int i = 0; i < sl; i++)
+	{
+		gotoXY(toadox[i], toadoy[i]);
+		cout <<" ";
+	}
+}
 
 void ve_tuong_tren()
 {
@@ -53,7 +120,6 @@ void ve_tuong_tren()
     x++;
   }
 }
-
 void ve_tuong_duoi()
 {
   int x = 10, y = 26;
@@ -63,7 +129,6 @@ void ve_tuong_duoi()
     x++;
   }
 }
-
 void ve_tuong_trai()
 {
   int x = 10, y=1;
@@ -73,7 +138,6 @@ void ve_tuong_trai()
     y++;
   }
 }
-
 void ve_tuong_phai()
 {
   int x = 100, y=1;
@@ -83,7 +147,6 @@ void ve_tuong_phai()
     y++;
   }
 }
-
 void ve_tuong()
 {
   ve_tuong_tren();
