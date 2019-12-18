7534499b218365c013189b7411584a4056dfc358
Phạm Đăng Đan <dangdan2807@gmail.com>
2019-12-18 04:53:30 +0000

    feat: Add the losing conditions of the game (Thêm các điều kiện thua của trò chơi)
    vn: thêm cái điều kiện để trò chơi kết thúc như: rắn cắn đuôi, rắn chạm tường.
    en: more conditions for the game to end such as: snake bite tail, snake hit the wall.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 #include "console.h"
+#include <stdlib.h>
+#include <windows.h>
 using namespace std;
 
 #define MAX 100;
 int sl = 4;
-//Khai báo các hàm cần dùng
+//================= Khai báo các hàm ==================
 void ve_tuong_tren();
 void ve_tuong_duoi();
 void ve_tuong_trai();
 void ve_tuong_phai();
 void ve_tuong();
-void khoi_tao_ran();
+void khoi_tao_ran(int toadox[], int toadoy[]);
 void xoa_du_lieu_cu(int toadox[], int toadoy[]);
-void ve_ran();
-void xu_li_ran(int toadox, int toadoy, int x, int y);
-void them(int toado, int vt);
-void xoa(int toado, int vt);
+void ve_ran(int toadox[], int toadoy[]);
+void xu_li_ran(int toadox[], int toadoy[], int x, int y);
+void them(int toado[], int vt);
+void xoa(int toado[], int vt);
+bool kt_ran_cham_tuong(int x0, int y0);
+bool kt_ran_cham_duoi(int toadox[], int toadoy[]);
+bool kt_ran(int toadox[], int toadoy[]);
 
+// ============== Hàm Main ===============
 int main() {
+  bool gameover = false;
   int toadox[MAX], toadoy[MAX];
   ve_tuong();
   khoi_tao_ran(toadox, toadoy);
   ve_ran(toadox, toadoy);
   int x= 50, y = 13;
   int check = 2;
-  while(true)
+  while(gameover == false)
   {
   	//xóa vị trí cũ (backspace)
   	xoa_du_lieu_cu(toadox, toadoy);
@@ -73,15 +80,17 @@ int main() {
   	{
   		x--; // qua trái
   	}
+    // xử lí rắn;
   	xu_li_ran(toadox, toadoy, x, y);
+    // kiểm tra
+    gameover = kt_ran(toadox, toadoy);
   	sleep(150);
   }
   _getch();
 }
 
-
-//Định nghĩa các hàm đã khao báo ở trên
-void xu_li_ran(int toadox, int toadoy, int x, int y)
+//============== Định nghĩa các hàm ===================
+void xu_li_ran(int toadox[], int toadoy[], int x, int y)
 {
 	//b1: thêm tọa độ mới vào đầu mảng
 	them(toadox, x);
@@ -92,18 +101,18 @@ void xu_li_ran(int toadox, int toadoy, int x, int y)
 	//b3: vẽ lại rắn mới
 	ve_ran(toadox, toadoy);
 }
-void them(int toado, int vt)
+void them(int toado[], int vt)
 {
 	for(int i = sl; i > 0; i--){
-		a[i] = a[i - 1];
+		toado[i] = toado[i - 1];
 	}
-	a[0] = vt;
+	toado[0] = vt;
 	sl++;
 }
-void xoa(int toado, int vt)
+void xoa(int toado[], int vt)
 {
 	for(int i = vt; i < sl; i++){
-		a[i] = a[i + 1];
+		toado[i] = toado[i + 1];
 	}
 	sl--;
 }
@@ -112,10 +121,11 @@ void khoi_tao_ran(int toadox[], int toadoy[])
   int x=50, y=13;
   for(int i=0; i<sl; i++){
     toadox[i] = x;
-    toadiy[i] = y;
+    toadoy[i] = y;
+    x--;
   }
 }
-void ve_ran(int toadox[], toadoy[])
+void ve_ran(int toadox[], int toadoy[])
 {
   for(int i= 0; i< sl; i++){
     gotoXY(toadox[i], toadoy[i]);
@@ -178,4 +188,51 @@ void ve_tuong()
   ve_tuong_duoi();
   ve_tuong_trai();
   ve_tuong_phai();
+}
+
+bool kt_ran_cham_tuong(int x0, int y0)
+{
+  // rắn chạm tường trên
+  if(y0 == 1 && (x0 >= 10 && x0 <= 100))
+  {
+    return true;//gameover
+  }
+  // rắn chạm tường dưới
+  else if(y0 == 26 && (x0 >= 10 && x0 <= 100))
+  {
+    return true;//gameover
+  }
+  // rắn chạm tường trái
+  else if(x0 == 10 && (y0 >= 1 && y0 <= 26))
+  {
+    return true;//gameover
+  }
+  // rắn chạm tường phải
+  else if(x0 == 100 && (y0 >= 1 && y0 <= 26))
+  {
+    return true;//gameover
+  }
+  return false;
+}
+bool kt_ran_cham_duoi(int toadox[], int toadoy[])
+{
+  for(int i = 0; i < sl; i++)
+  {
+    if((toadox[0] == toadox[i]) && (toadoy[0] == toadoy[i]))
+    {
+      return true; //gameover
+    }
+  }
+  return false;
+}
+bool kt_ran(int toadox[], int toadoy[])
+{
+  // kiểm tra
+    bool kt1 = kt_ran_cham_duoi(toadox, toadoy);
+    bool kt2 = kt_ran_cham_tuong(toadox[0], toadoy[0]); 
+    if(kt1 == true || kt2 == true)
+    {
+      return true; // gameover
+    }
+    return false;
 }
